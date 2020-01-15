802ab95510de63d827b7d394f68e0bea81c2908c
Deanna Kelley <deanna.georges@gmail.com>
2020-01-15 17:29:43 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -17,7 +17,6 @@ class Application
           else
             @@cart.each do |item|
             resp.write "#{item}\n"
-          end
       elsif req.path.match(/search/)
           search_term = req.params["q"]
           resp.write handle_search(search_term)
